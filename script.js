927fee928bb03f8946a5a156e252b0e0d3e81c7e
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-04-01 10:33:37 -0300

    exercicios ate 19

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ function firstForm() {
   }
 }
 buttonSubmit.addEventListener('click', firstForm);
+
+
+//*função que desabilita o botão inicialmente e habilita após o checkbox
+let check = document.querySelector('#agreement');
+let button = document.querySelector('#submit-btn');
+button.disabled = true;
+
+check.addEventListener('change', stateHandle);
+function stateHandle() {
+
+  if (check.value === true) {
+    button.disabled = true;
+  } else {
+    button.disabled = false;
+  }
+}
