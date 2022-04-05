8c19e05862e2e3b7f2f3d84d26d2faaf96cadaec
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-04-04 23:22:17 -0300

    tentando resolver o exercicio21, faltam 3 elementos para serem impressos

--- script.js
+++ script.js
@@ -39,4 +39,37 @@ typingTextArea.addEventListener('keyup', counterTyping);
 
 // exercicio 21 -  ao clicar no botao enviar, alterar o conteudo do form para os itens preenchidos
 
+const delCont = document.getElementById('submit-btn');
+delCont.addEventListener('click', changeValue);
+const form = document.getElementById('evaluation-form');
 
+function changeValue () {
+  const name = document.getElementById('input-name');//ok
+  const email = document.getElementById('input-email');//ok
+  const lastName = document.getElementById('input-lastname');//ok
+  const casa = document.getElementById('house');//ok
+  const familia = document.getElementById('label-family'); //! nào recebe
+  // const materias = document.getElementById('label-content') //! nào recebe
+  const avaliacao = document.getElementById('label-rate'); //! nào recebe
+  const observacoes = document.getElementById('textarea'); //ok 
+
+  form.innerHTML = '';
+  form.innerText = 
+  `Nome: ${name.value} ${lastName.value}
+  Email: ${email.value}
+  Casa: ${casa.value} Escolhida
+  Família: ${familia.value} Escolhida
+  Matérias: ${materials}
+  Avaliação: ${avaliacao.value}
+  Observações: ${observacoes.value}`;  
+}
+
+const materiais = document.getElementsByClassName('subject');
+let materials = 0;
+  
+  for (let index = 0; index < materiais.length; index += 1) {
+    if (materials[index].checked) {
+      materials = materiais[index];
+    }
+  } 
+ 
